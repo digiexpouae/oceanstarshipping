52784828bda1aed95dae6d48ae95996349f445c3
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-10 19:39:44 +0500

    ocean star

--- src/components/home/blog_section.jsx
+++ src/components/home/blog_section.jsx
@@ -3,7 +3,7 @@ import Blogs from '@/common/blogs'
 import Link from 'next/link'
 const blog_section = () => {
   return (
-    <div >
+    <div className='mb-20' >
             <h3 className=' text-[22px] text-primary md:text-[50px] underline mb-3 md:mb-8 font-heading font-bold text-center'>OUR BLOGS</h3>
       <Blogs />
       <div className='flex items-center justify-center w-full'> <button className='btn w-[130px] '><Link href='/blogs'>View All Blogs</Link></button></div>
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -12,7 +12,7 @@ const index = () => {
   return (
   <>
         <Header />
-  <div className='flex flex-col justify-between gap-35'>
+  <div className='flex flex-col justify-between gap-15'>
 
    <Herobanner />
    <Slider />
