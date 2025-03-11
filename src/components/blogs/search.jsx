88488242021ff77b2fcd50af5672c8754a955ac4
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-11 19:20:39 +0500

    ocean

--- src/components/blogs/search.jsx
+++ src/components/blogs/search.jsx
@@ -4,13 +4,13 @@ import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"; //
 const search = () => {
     return (
         <div className="w-full flex justify-center mt-[10px]">
-          <div className="flex border border-primary items-center w-[70%] md:w-[50%] px-2 py-1 md:py-3">
+          <div className="flex border border-primary items-center w-[70%] md:w-[50%] px-2  py-1 md:py-3">
             <FontAwesomeIcon icon={faMagnifyingGlass} className="mr-2 md:px-3" />
             <div className="w-full">
               <input type="text" className="w-full outline-none" placeholder="Search Blogs" />
             </div>
           </div>
-        </div>
+        </div>  
       );
     };
     
