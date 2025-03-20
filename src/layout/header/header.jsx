3e5e9c85e31c30d931c05c85d5c5606349a1cbce
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-20 22:59:02 +0500

    ocean star

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -12,8 +12,8 @@ const router =useRouter();
 
  const handlelogout=async ()=>{
   await fetch('api/logout',{method:'POST'})
-  router.push('/admin')
- }
+  router.push('/admin')}
+ 
 
 
 
@@ -30,7 +30,7 @@ const router =useRouter();
          ' onClick={()=>{(setopen(!open))}}><FontAwesomeIcon icon={faBars} className="text-xl" />
         </a>
       {/* mobile nav */}
-        <div className={`fixed h-full w-3/4 duration-300 ease-in-out flex flex-col pt-14 px-10 items-center leading-16  bg-[#F6F6F6] right-[-40px] top-0 bottom-0 z-50   ${open ? "block" : "translate-x-96"}`}>
+        <div className={`fixed h-full w-3/4 duration-300 ease-in-out flex flex-col pt-14 px-10 items-center leading-16  bg-[#F6F6F6] right-[-40px] top-0 bottom-0 z-50   ${open ? "block" : "translate-x-[525px]"}`}>
         <a className='absolute h-[35px] w-[35px] bg-white left-[20px] top-[10px] flex justify-center items-center text-xl bold' onClick={()=>{(setopen(false))}}> <FontAwesomeIcon icon={faXmark} /></a>
         <div className=' w-full '><Link href='/'><Image src={logo}     alt="logo"
         width={100} height={80}
@@ -53,7 +53,7 @@ const router =useRouter();
     <ul className=' hidden md:flex !justify-between w-96' >
       
     {nav.map((elem,index)=>{
-   return  <li  key={index} className='text-black transition-all duration-300  hover:text-primary hover:font-bold cursor-pointer'><Link href={elem.link}>{elem.name}</Link></li>
+   return  <li  key={index} className='text-black hover:text-primary hover:font-bold cursor-pointer'><Link href={elem.link}>{elem.name}</Link></li>
 
 })    
 }   
