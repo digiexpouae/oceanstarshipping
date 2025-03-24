babea7effcd23bd8363d14de71cac62ac6e0676b
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-24 14:51:54 +0500

    package

--- src/pages/blogs/[slug].jsx
+++ src/pages/blogs/[slug].jsx
@@ -1,71 +1,59 @@
-import { blog_data } from "@/common/blogs";
-import Header from "@/layout/header/header";
-import Footer from "@/layout/footer/footer";
-import Feature from "@/common/feature";
-import Bannertwo from "@/common/bannertwo";
-import Herobanner from "@/common/herobanner";
-import imagetwo from '../../../public/assets/Rectangle 34 (6).png'
-import { client } from "@/sanity/lib/client";
-import { urlFor } from "@/sanity/lib/image";
-// export async function getStaticPaths() {
-//     const paths = blog_data.map((blog) => ({
-//       params: { slug: blog.slug }, // Each blog's ID is used to create a dynamic path
-//     }));
-  
-//     return { paths, fallback: false }; // No new paths can be created at runtime
-//   }
-  
-export async function getServerSideProps(context){
-const { slug } = context.params
-  // Fetch all blogs sorted by publishedDate
-  const query = `*[_type == "blog"] | order(publishedDate asc) {
-    title, "slug":slug.current, content, "imageUrl": Image.asset._ref, publishedDate
-  }`;
-  
-  try {
-    const blogs = await client.fetch(query); // Get all blogs
-    // Find the index of the current blog
+// import { blog_data } from "@/common/blogs";
+// import Header from "@/layout/header/header";
+// import Footer from "@/layout/footer/footer";
+// import Feature from "@/common/feature";
+// import Bannertwo from "@/common/bannertwo";
+// import Herobanner from "@/common/herobanner";
+// import imagetwo from '../../../public/assets/Rectangle 34 (6).png'
+// import { client } from "@/sanity/lib/client";
+// import { urlFor } from "@/sanity/lib/image";
 
-    const blogIndex = blogs.findIndex((b) => b.slug === slug);
+  
+// export async function getServerSideProps(context){
+// const { slug } = context.params
+//   // Fetch all blogs sorted by publishedDate
+//   const query = `*[_type == "blog"] | order(publishedDate asc) {
+//     title, "slug":slug.current, content, "imageUrl": Image.asset._ref, publishedDate
+//   }`;
+  
+//   try {
+//     const blogs = await client.fetch(query); // Get all blogs
+//     // Find the index of the current blog
+
+//     const blogIndex = blogs.findIndex((b) => b.slug === slug);
  
-    // If blog not found, return 404
-    if (blogIndex === -1) {
-      return { notFound: true };
-    }
+//     // If blog not found, return 404
+//     if (blogIndex === -1) {
+//       return { notFound: true };
+//     }
 
-    // Get current blog, previous blog, and next blog
-    const blog = blogs[blogIndex]; // Current blog (single object)
-    const prevBlog = blogs[blogIndex - 1] ?? blogs[blogs.length - 1]; 
-    const nextBlog = blogs[blogIndex + 1] ?? blogs[0];
-    console.log(blogs[blogIndex + 1])
-    return {
-      props: {
-        blog,       // Single blog object
-        prevBlog,   // Previous blog (if exists)
-        nextBlog    // Next blog (if exists)
-      }
-    };
-  } catch (error) {
-    console.error("Error fetching data from Sanity:", error);
-    return {
-      props: {
-        blog: null,
-        prevBlog: null,
-        nextBlog: null
-      }
-    };
-  }
-}
-
-
-
-// export async function getStaticProps({params}){
-//     const dynamic_blog=(blog_data.find((p)=>p.slug == params.slug))
-//     console.log(dynamic_blog)
-//     return {props: {dynamic_blog}}
-
+//     // Get current blog, previous blog, and next blog
+//     const blog = blogs[blogIndex]; // Current blog (single object)
+//     const prevBlog = blogs[blogIndex - 1] ?? blogs[blogs.length - 1]; 
+//     const nextBlog = blogs[blogIndex + 1] ?? blogs[0];
+//     console.log(blogs[blogIndex + 1])
+//     return {
+//       props: {
+//         blog,       // Single blog object
+//         prevBlog,   // Previous blog (if exists)
+//         nextBlog    // Next blog (if exists)
+//       }
+//     };
+//   } catch (error) {
+//     console.error("Error fetching data from Sanity:", error);
+//     return {
+//       props: {
+//         blog: null,
+//         prevBlog: null,
+//         nextBlog: null
+//       }
+//     };
+//   }
 // }
-import { useRouter } from "next/router";
+
+
+
+// import { useRouter } from "next/router";
 // const Blog_post = ({blog,prevBlog,nextBlog}) => {
 //     const router=useRouter()
 //     const {title,imageUrl,content,publishedDate,slug}=blog
@@ -91,4 +79,4 @@ import { useRouter } from "next/router";
 //   )
 // }
 
-export default Blog_post
+// export default Blog_post
