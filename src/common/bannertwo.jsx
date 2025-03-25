87944d4cb87667d044ab5f4aa8505ca9306f6d59
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-25 11:12:31 +0500

    ocean star

--- src/common/bannertwo.jsx
+++ src/common/bannertwo.jsx
@@ -12,7 +12,7 @@ const bannertwo = () => {
  <div className='absolute left-[10%]  md:left-[10%]  md:w-[475px] flex flex-col md:gap-[10px] lg:gap-[30px]'><h3 className='uppercase text-[14px] font-extrabold  md:text-[27px] lg:text-[45px] leading-5 md:leading-9 lg:leading-13 font-heading text-primary'>Get in
 <br />Touch For
 <br/>Any Queries</h3>
-<button className='btn'><Link href='/contact'>Contact us</Link></button>
+<button className='btn-banner'><Link href='/contact'>Contact us</Link></button>
 </div>
     </div>
   )
