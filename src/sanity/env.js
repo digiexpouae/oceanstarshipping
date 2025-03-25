72e8f9374cf1eafeeddb3e092b17c8e0465e1f94
ramsha1993 <rimshasaleem1993@gmail.com>
2025-03-25 11:12:54 +0500

    ocean star

--- src/sanity/env.js
+++ src/sanity/env.js
@@ -2,5 +2,5 @@ export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-03-12'
 
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'o5jomau3'  
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID 
 export const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
