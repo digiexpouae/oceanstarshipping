0f432f50f11b95bf6b468bf8c09f6bba48d77023
ramsha1993 <rimshasaleem1993@gmail.com>
2025-04-15 12:53:31 +0500

    link

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -6,7 +6,7 @@ export const client = createClient({
 //   projectId :'6kvdqvro', // Replace with actual Project ID
 // dataset :'production' ,// Example: 'production'
 // apiVersion : "2025-03-12", ,
-apiVersion:NEXT_PUBLIC_SANITY_API_VERSION, dataset:NEXT_PUBLIC_SANITY_DATASET, projectId:NEXT_PUBLIC_SANITY_PROJECT_ID,
+apiVersion, dataset:NEXT_PUBLIC_SANITY_DATASET, projectId:NEXT_PUBLIC_SANITY_PROJECT_ID,
 
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
