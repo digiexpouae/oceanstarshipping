a6ef7f26aabb08f96685de4d217f353765b59702
ramsha1993 <rimshasaleem1993@gmail.com>
2025-04-15 11:17:54 +0500

    sap

--- src/common/bannertwo.jsx
+++ src/common/bannertwo.jsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 const bannertwo = () => {
   return (
     <div className='w-full flex items-center justify-center relative h-[125px] md:h-[330px] xl:h-[408px]'>
-<div className='absolute z-[-10]  top-[-50%] md:top-[-30%] lg:top-[-40%]  xl:top-[-35%]   right-[1%] '>      
-<div className='h-[100px] w-[120px] md:h-[280px] md:w-[265px] relative'><Image src={star} layout='intrinsic' /></div></div>
+<div className='absolute z-[-10]  top-[-30%] md:top-[-30%] lg:top-[-40%]  xl:top-[-35%]   right-[1%] hidden lg:flex'>      
+<div className='h-[85px] w-[85px] md:h-[220px] md:w-[220px] lg:h-[280px] lg:w-[265px] relative'><Image src={star} layout='intrinsic' /></div></div>
 <div className='w-[85%] relative '><Image  src={poster} layout='responsive' /></div>   
  <div className='absolute left-[10%]  md:left-[10%]  md:w-[475px] flex flex-col md:gap-[10px] lg:gap-[30px]'><h3 className='uppercase text-[14px] font-extrabold  md:text-[27px] lg:text-[45px] leading-5 md:leading-9 lg:leading-13 font-heading text-primary'>Get in
 <br />Touch For
--- src/components/home/products.jsx
+++ src/components/home/products.jsx
@@ -1,31 +1,65 @@
-import React from 'react'
-import Image from 'next/image'
-import headphones from '../../../public/assets/headphone.png'
-import mobilephone from '../../../public/assets/pngegg.png'
-import tablets from '../../../public/assets/pngegg (1).png'
-import LED from '../../../public/assets/Led.png'
-import playstation from '../../../public/assets/erasebg-transformed.png'
-import arrow from '../../../public/assets/Vector (6).svg'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import headphones from '../../../public/assets/headphone.png';
+import mobilephone from '../../../public/assets/pngegg.png';
+import tablets from '../../../public/assets/pngegg (1).png';
+import LED from '../../../public/assets/Led.png';
+import playstation from '../../../public/assets/erasebg-transformed.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
+import Link from 'next/link';
 
-import Link from 'next/link'
-const products = () => {
-  
-    const images=[{image:headphones,link:'/',title:'Headphones'},{image:mobilephone,link:'/',title:'Mobile phones'},{image:tablets,link:'/',title:'Tablets'},
-        {image:LED,link:'/',title:'LED'},{image:playstation,link:'/',title:'Other Accessorries'}]
+const Products = () => {
+  const images = [
+    { image: headphones, link: '/', title: 'Headphones', description: 'product description' },
+    { image: mobilephone, link: '/', title: 'Mobile phones', description: 'product description' },
+    { image: tablets, link: '/', title: 'Tablets', description: 'product description' },
+    { image: LED, link: '/', title: 'LED', description: 'product description' },
+    { image: playstation, link: '/', title: 'Other Accessories', description: 'product description' }
+  ];
+
+  const [openIndex, setOpenIndex] = useState(null);
+
   return (
-    <div className='h-full'>
-        <h3 className=' text-[22px] text-primary md:text-[50px] underline mb-3 md:mb-8 font-heading font-bold text-center'>PRODUCTS</h3>
-      <div className=' flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-3 place-items-center w-full'>
-        {images.map((elem,index)=>{
-       return <div className=' bg-[#F6F6F6] w-[300px] h-[260px] md:h-[300px]  xl:w-[340px] lg:h-[300px]  flex flex-col justify-center items-center'> 
-       <div key={index} className='h-[85%] w-[220px]  md:w-[270px] xl:w-[268px] flex flex-col justify-center items-center'><Image src={elem.image} /> </div>
-<Link href={elem.link} className='h-[15%] md:text-start w-full py-6 px-[18px] text-primary text-start flex items-center font-bold' >{elem.title}</Link>
+    <div className="h-full">
+      <h3 className="text-[22px] text-primary md:text-[50px] underline mb-3 md:mb-8 font-heading font-bold text-center">
+        PRODUCTS
+      </h3>
+      <div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3 gap-x-[2px] gap-y-15 place-items-center w-full mb-[17px] md:mb-0">
+        {images.map((elem, index) => (
+          <div
+            key={index}
+            className="bg-[#F6F6F6] w-[300px] h-[260px] md:h-[300px] xl:w-[370px] lg:h-[300px] flex flex-col justify-end items-center relative
+            hover:shadow-lg transition duration-200 ease-in-out cursor-pointer"
+          >
+            <div className="h-[85%] w-[220px] md:w-[270px] xl:w-[268px] flex flex-col justify-center items-center">
+              <Image src={elem.image} alt={elem.title} />
+            </div>
 
-       </div>
-})}
-    </div>
-    </div>
-  )
-}
+            <div
+              className="flex justify-between items-center w-full px-4 h-[20%] cursor-pointer"
+              onClick={() => setOpenIndex(openIndex === index ? null : index)}
+            >
+              <div
+                
+                className="h-[15%] md:text-start w-full py-6 px-[18px] text-primary text-start flex items-center font-bold"
+              >
+                {elem.title}
+              </div>
+             <div className='w-4 h-4 block'> <FontAwesomeIcon icon={faCaretDown} color="#161647" className="cursor-pointer border-box w-full h-full !overflow-hidden" /></div>
+            </div>
 
-export default products
+            {openIndex === index && (
+              <div className="absolute top-[100%] h-16 left-0 right-0 bottom-0 rounded-br-xl rounded-bl-xl
+               bg-[#F6F6F6] shadow-md flex items-center justify-center p-0 z-10">
+                {elem.description}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
